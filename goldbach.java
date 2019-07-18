4bd765029616eed925417dc8e929b1300cc826d0
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-07-18 16:26:04 -0400

    Update goldbach.java

--- goldbach.java
+++ goldbach.java
@@ -7,49 +7,47 @@ return all of the primes of primes numbers that sum to that number.
 */
 public class GoldBach {
 
-
-    //method to determine if a given number is prime
-    public static boolean isPrime(int a) {
-        if (a<2) {
-            return false;
-    } else {
-            for (i=2; i<a; i++){
-                if (a%i==0) {
-                    return false;
-                } else {
-                    return true;
-                }
-                }
-            }
-        }
-    //method to create array list of prime numbers up to n
-    public static ArrayList<Integer> primesFinder(int n) {
-        List<Integer> primes = new ArrayList<>();
-            for (i=2; i<=n; i++) {
-                if (isPrime(i)=true){
-                    primes.add(i);
-            }
-        return primes;
-    }
-
-
-
     public static void main(String[] args){
-        System.out.println("The Goldbach conjecture is...");
-        System.out.println("What number would you like to find all the pairs of prime numbers that sum to it?");
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        if (n<=2) {
-            System.out.println("The number must be great than 2!");
-        } else if (n%2!=0){
-            System.out.println("The number must be even!");
-        } else {
+            System.out.println("The Goldbach conjecture is...");
+            System.out.println("What number would you like to find all the pairs of prime numbers that sum to it?");
+            Scanner input = new Scanner(System.in);
+            int n = input.nextInt();
+            if (n<=2) {
+                System.out.println("The number must be great than 2!");
+            } else if (n%2!=0){
+                System.out.println("The number must be even!");
+            } else {
+                ArrayList<Integer> primeslist = new ArrayList<Integer>;
+                primeslist=primesFinder(n);
 
-            primes[] = primesFinder(n);
 
             }
             System.out.println(primes);
         }
 
     }
+
+    //method to determine if a given number is prime
+    static boolean isPrime(int a) {
+        if (a<2) {
+            return false;
+        } else {
+            for (i=2; i<a; i++){
+                if (a%i==0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
+    //method to create array list of prime numbers up to n
+    static ArrayList<Integer> primesFinder(int n) {
+        List<Integer> primes = new ArrayList<>();
+        for (i=2; i<=n; i++) {
+            if (isPrime(i)=true){
+                primes.add(i);
+            }
+            return primes;
+        }
 }
